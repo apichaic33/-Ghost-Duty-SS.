453bc44dee13c568496ce8f4bb1188d30a1daf28
apichaic33 <q.apichai@gmail.com>
2026-04-20 20:38:42 +0700

    auto: update /Users/laboon/Documents/GitHub/-Ghost-Duty-SS./src/components/Settings.tsx [2026-04-20 20:38]

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { doc, updateDoc, collection, onSnapshot, setDoc, deleteDoc, getDoc } from 'firebase/firestore';
+import { doc, updateDoc, collection, onSnapshot, setDoc, deleteDoc, getDoc, getDocs } from 'firebase/firestore';
 import { db } from '../firebase';
 import { Member, NotificationPreferences, ShiftProperty } from '../types';
 import { toast } from 'sonner';
