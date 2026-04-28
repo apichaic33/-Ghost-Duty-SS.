792956bfcc2764a2ec975ae420bc389e323895b3
apichaic33 <q.apichai@gmail.com>
2026-04-28 11:35:03 +0700

    auto: update /Users/laboon/Documents/GitHub/-Ghost-Duty-SS./src/components/TeamSchedule.tsx [2026-04-28 11:35]

--- src/components/TeamSchedule.tsx
+++ src/components/TeamSchedule.tsx
@@ -227,7 +227,7 @@ export default function TeamSchedule({ member, isAdmin }: TeamScheduleProps) {
         <table className="border-collapse text-xs" style={{ tableLayout: 'fixed', minWidth: '100%' }}>
           <thead>
             <tr className="bg-gray-50 border-b border-gray-200">
-              <th className="sticky left-0 z-20 bg-gray-50 px-3 py-3 text-left font-bold text-gray-500 uppercase border-r border-gray-200" style={{ minWidth: 170, width: 170 }}>
+              <th className="sticky left-0 z-20 bg-gray-50 px-2 py-3 text-left font-bold text-gray-500 uppercase border-r border-gray-200" style={{ minWidth: 96, width: 96 }}>
                 สมาชิก
               </th>
               {days.map(day => (
